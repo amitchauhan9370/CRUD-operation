61ab10144a7a1d5e5bd44d7bc883bbf36cc38dd0
amitchauhan9370 <160030713+amitchauhan9370@users.noreply.github.com>
2026-06-22 12:29:17 +0530

    Updated project files

--- models/user.js
+++ models/user.js
@@ -1,6 +1,26 @@
-const mongoose = require('mongoose');
+// const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/testapp1");
+// mongoose.connect(process.env.MONGO_URL)
+// .then(() => console.log("DB connected"))
+// .catch(err => console.log("DB error:", err));
+
+// const userSchema = mongoose.Schema({
+//     image: String,
+//     email: String,
+//     name : String
+// });
+
+// module.exports = mongoose.model('user', userSchema);
+
+
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+console.log("MONGO_URI =", process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB connected"))
+.catch(err => console.log("DB error:", err));
 
 const userSchema = mongoose.Schema({
     image: String,
@@ -9,3 +29,5 @@ const userSchema = mongoose.Schema({
 });
 
 module.exports = mongoose.model('user', userSchema);
+
+
